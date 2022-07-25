e02dc407470a063b4cd8661e3c94f0ba91dcdae4
playa <accurateandcool@gmail.com>
2022-07-25 09:36:47 +0300

    getStaticProps

--- pages/[id].jsx
+++ pages/[id].jsx
@@ -1,10 +1,21 @@
 import { useRouter } from "next/router"
 
 
-export default function(){
+export default function({user}){
     const {query}  = useRouter()
 
     return(
-        <h1>todo {query.id}</h1>
+        <div>
+            <h1>todo {query.id}</h1>
+            <h2>name is {user.name}</h2>
+        </div>
+        
     )
 }
+export async function getServerSideProps({params}) {
+    const response = await fetch(`https://jsonplaceholder.typicode.com/users/${params.id}`)
+    const user = await response.json()
+    return {
+      props: {user}
+    }
+  }
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,8 @@
 import Link from 'next/link'
-const index = () => {
+import { useState, useEffect } from "react"
 
+const index = ({users}) => {
+    
     return (
         <div>
             <Link href="/statistics">
@@ -8,9 +10,21 @@ const index = () => {
             </Link>
 
             <h1>index.jsx next only</h1>
-            <h2>btw I lost build folder</h2>
+            {users.map(todo=>
+            <li key={todo.id}>
+                <Link href={`/${todo.id}`}><a>{todo.name}</a></Link>
+            </li>
+            
+            )}
         </div>
     )
 }
 
 export default index
+export async function getStaticProps(context) {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users')
+    const users = await response.json()
+    return {
+      props: {users}
+    }
+  }
